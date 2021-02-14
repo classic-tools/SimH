b3d9c4b07af1b7d3e06ea8bb47fcad9d103a9fcd
Gerardo Ospina <ngospina@gmail.com>
2021-02-14 00:07:51 -0500

    SimH 3.11-0
    02/13/2020

--- SimH V3/sim_rev.h
+++ SimH V3/sim_rev.h
@@ -28,18 +28,36 @@
 #define _SIM_REV_H_     0
 
 #define SIM_MAJOR       3
-#define SIM_MINOR       10
+#define SIM_MINOR       11
 #define SIM_PATCH       0
 #define SIM_DELTA       0
+
+/* V3.11 revision history
+
+   V3.11 incorporates SCP additions and extensions by Dave Bryan to support his
+   HP simulators.
+
+patch   date            module(s) and fix(es)
+
+  0     01-Dec-2019     scp.c and supporting libraries
+                        - new features and extensions to support the HP simulators
+
+                        sim_sock.c
+                        - latest bug fixes from V4.x
+
+                        VAX
+                        - added hook for unpredictable indexed immediate in .aw flows
+                        - revamped Qbus memory simulation to behave as a Qbus peripheral
+*/
 
 /* V3.10 revision history
 
    V3.10 backports an significant number of modules and features from
-   the SimH 4.x master branch, as well as some unique new features.
+   the SimH 4.x branch, as well as some unique new features.
 
 patch   date            module(s) and fix(es)
 
-  0     tbd             scp.c and supporting libraries
+  0     24-Feb-2019     scp.c and supporting libraries
                         - added sim_printf
                         - added ATTACH -N switch
                         - added v4 compatibility macros
